94b03935766abb7885bdf593d2fd6182a07d4cde
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2021-01-03 18:40:52 +0500

    1. Servlet. Web.xml [#430861]

---
+++ src/main/java/controller/PostServlet.java
@@ -0,0 +1,20 @@
+package controller;
+
+import model.Post;
+import store.Store;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+public class PostServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        Store.instOf().save(new Post(0, req.getParameter("name"),"", new Date()));
+        resp.sendRedirect(req.getContextPath() + "/posts.jsp");
+    }
+}
--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -7,12 +7,15 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
     private static final Store INST = new Store();
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private static AtomicInteger POST_ID = new AtomicInteger(4);
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "", new Date()));
@@ -34,4 +37,10 @@ public class Store {
     public Collection<Post> findAllPosts() {
         return posts.values();
     }
+
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
+    }
+
 }
