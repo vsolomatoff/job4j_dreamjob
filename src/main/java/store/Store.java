fca10b7456967a2fcc0d4e1bf6901a5a73c6bf7a
Solomatov Viacheslav <solomatoff.studio@gmail.com>
2021-01-03 17:22:04 +0500

    4. candicates.jsp - список кандидатов. [#430850]

--- src/main/java/store/Store.java
+++ src/main/java/store/Store.java
@@ -1,5 +1,6 @@
 package store;
 
+import model.Candidate;
 import model.Post;
 
 import java.util.Collection;
@@ -10,19 +11,27 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Store {
     private static final Store INST = new Store();
 
-    private Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job", "", new Date()));
         posts.put(2, new Post(2, "Middle Java Job", "", new Date()));
         posts.put(3, new Post(3, "Senior Java Job", "", new Date()));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
+    }
+
+    public Collection<Post> findAllPosts() {
         return posts.values();
     }
 }
